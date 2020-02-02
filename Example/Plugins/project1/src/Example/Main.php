088b91a2c47da973c9c91c4ad80100e3d18c4384
SaveConnectionPE <59916034+SaveConnectionPE@users.noreply.github.com>
2020-02-02 02:42:40 +0100

    Create Main.php

---
+++ Example/Plugins/project1/src/Example/Main.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Example;
+
+use pocketmine\Server;
+use pocketmine\Player;
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+
+class Main extends PluginBase implements Listener{
+  
+   public function onEnable(){
+    $this->getServer()->getPluginManager()->registerEvents($this, $this);
+    $this->getLogger()->info("Plugin enabled!");
+  }
+  
+  public function onDisable(){
+    $this->getLogger()->info("Plugin disabled!");
+  }
